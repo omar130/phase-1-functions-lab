44035c40bf3f129b9db8ec4608a4f8d896cb6304
Omar Richards <omarrichards01@gmail.com>
2023-06-12 22:51:29 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(someValue){
+  
+  if (someValue > 42) {
+    return someValue - 42
+  } else if (someValue < 42){
+    return 42 - someValue
+  }
+  
+}
+
+function distanceFromHqInFeet(someValue){
+  distanceFromHqInBlocks(someValue); {
+     return distanceFromHqInBlocks(someValue) * 264
+  }
+
+  }
+    
+function distanceTravelledInFeet (start, destination) {
+ 
+  return Math.abs(destination - start) * 264 
+
+}
+
+
+function calculatesFarePrice(start, destination) {
+  distanceTravelledInFeet (start, destination); {
+    
+      return (calculatesFarePrice - 400) * 0.02;
+    }
+  }
+
+
